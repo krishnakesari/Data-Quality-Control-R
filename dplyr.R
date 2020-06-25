027bfd859e07c04863f06ecd49c6efa85e55b42b
Krishna <bkesari@outlook.com>
2020-06-25 12:19:02 -0400

    Manipulation

--- dplyr.R
+++ dplyr.R
@@ -1,2 +1,10 @@
 head(iris)
 
+iris.setosa <- subset(iris, Species == "setosa",
+                    select=c(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width))
+head(iris.setosa)
+
+# Calculating mean for the column values
+setosa <- colMeans(iris.setosa)
+setosa
+
