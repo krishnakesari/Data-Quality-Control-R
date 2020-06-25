65b95f00dc4638e69b5a6e677aa289f2adc61779
Krishna <bkesari@outlook.com>
2020-06-25 13:27:00 -0400

    Manipulate

--- Using Plyr.R
+++ Using Plyr.R
@@ -10,3 +10,22 @@ mean.Titanic.loop <- NULL
     }
 
 rownames(mean.Titanic.loop) <- unique(Titanic$Sex)
+
+# Using plyr utilities
+library(plyr)
+ddply(iris, .(Species), function(x) colMeans(x[-5]))
+ddply(iris, .(Species), function(x) sum(x[-5]))
+
+class(iris3)
+dim(iris3)
+
+# calculate column mean for each species and output will be a data frame
+iris_mean <- adply(iris3,3, colMeans)
+iris_mean
+class(iris_mean)
+
+# Calculate column mean and out put will be an array/matrix
+iris_mean2 <- aaply(iris3,3, colMeans)
+iris_mean2
+class(iris_mean2)
+
