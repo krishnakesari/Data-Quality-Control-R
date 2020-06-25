78147b51c85956beca9e8d757035139c82b68525
Krishna <bkesari@outlook.com>
2020-06-25 12:26:18 -0400

    Manipulation

--- dplyr.R
+++ dplyr.R
@@ -6,5 +6,15 @@ head(iris.setosa)
 
 # Calculating mean for the column values
 setosa <- colMeans(iris.setosa)
-setosa
 
+# Splitting dataset
+iris.split <- split(iris, as.factor(iris$Species))
+head(iris.split)
+
+## Calculating mean value for the splitting dataset
+iris.apply <- lapply(iris.split, function(x)colMeans(x[-5]))
+iris.apply
+
+# Combining results
+iris.combine <- do.call(rbind,iris.apply)
+iris.combine
